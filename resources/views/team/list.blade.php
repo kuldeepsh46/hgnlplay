9a4a5131149dc0933798e8eb3591ca50dbec245a
Kuldeep Sharma <sh.kuldeep46@gmail.com>
2026-06-09 08:07:28 -0400

    Debugging list issue

--- resources/views/team/list.blade.php
+++ resources/views/team/list.blade.php
@@ -239,7 +239,7 @@
             </thead>
 
             <tbody>
-                {{dd($teamMembers)}}
+                {{-- {{dd($teamMembers)}} --}}
                 @foreach ($teamMembers as $member)
                     {{-- {{dd($member)}} --}}
                     @php
